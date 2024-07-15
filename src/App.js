134643d5b6742e646e1c25b291c53e45ae6d7469
Udhaya <udhyakumar162000@gmail.com>
2024-07-15 22:19:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import Navbar from './pages/Navbar.js';
 import Newpage from './pages/Newpage.js';
+import Form from './pages/Form.js';
 
 function App() {
   return (
     <div className="App">
-    
+    <Form/>
   
     </div>
   );
